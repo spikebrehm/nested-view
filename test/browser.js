eee2db3ef46be546c1da16db4c3e5cc59e41ac4d
Spike Brehm <ocelot@gmail.com>
2012-10-21 17:54:49 -0700

    adding browser tests

---
+++ test/browser.js
@@ -0,0 +1,84 @@
+(function(){
+  var assert = chai.assert
+    , views = {}
+    , templates = {}
+    , userData = {first_name: 'Spike', last_name: 'Brehm'}
+    , listingData = {name: 'Cozy downtown', user: userData};
+
+  NestedView.registerHandlebars(Handlebars);
+
+  var BaseNestedView = NestedView.extend({
+    getTemplate: function(){
+      return templates[this.name];
+    }
+  });
+
+  NestedView.getView = function(name) {
+    return views[name];
+  };
+
+  templates['listing_view'] = Handlebars.compile(
+    '<h1>Listing: {{name}}</h1>' +
+    '{{view "user_view" context=user}}'
+  );
+
+  templates['user_view'] = Handlebars.compile(
+    '<h2>User: {{first_name}} {{last_name}}</h2>'
+  );
+
+  views['listing_view'] = BaseNestedView.extend({
+    name: 'listing_view',
+    tagName: 'section',
+    id: 'listing',
+    className: 'listing',
+    postRender: function(){
+      var _this = this;
+      this.childViews['user_view'].on('clicked', function(e){
+        _this.trigger('child_clicked', e);
+        console.log(_this, 'child_clicked');
+      });
+    }
+  });
+
+  views['user_view'] = BaseNestedView.extend({
+    name: 'user_view',
+    className: 'user',
+    events: {'click':'click'},
+    click: function(e){
+      this.trigger('clicked', e);
+      console.log(this, 'clicked');
+    }
+  });
+
+  describe('Browser', function(){
+    describe('rendering', function(){
+      it('should render the same html that getHtml() returns', function(){
+        var listingView = new views['listing_view'](listingData)
+          , renderedInnerHtml = listingView.render().$el.html()
+          , renderedOuterHtml = listingView.el.outerHTML
+          , innerHtml = listingView.getHtml({outerHtml: false})
+          , outerHtml = listingView.getHtml();
+
+        assert.equal(renderedInnerHtml, innerHtml);
+        assert.equal(renderedOuterHtml, outerHtml);
+      });
+    });
+
+    describe('childViews', function(){
+      it('should be able to listen to events emitted by the child', function(done){
+        var listingView = new views['listing_view'](listingData);
+
+        listingView.on('child_clicked', function(e){
+          assert(true);
+          assert.equal(e.currentTarget === listingView.childViews['user_view'].el);
+          done();
+        });
+
+        $('#container').html(listingView.render().el);
+
+        $('.user').click();
+      });
+    });
+  });
+
+})();
---
+++ test/main.js
@@ -0,0 +1,113 @@
+(function(){
+  var server = !this.window;
+
+  var assert =  server ? require('assert') : this.chai.assert
+    , Handlebars = server ? require('handlebars') : this.Handlebars
+    , NestedView = server ? require('../index') : this.NestedView
+    , views = {}
+    , templates = {}
+    , userData = {name: 'Spike', email: 'spike@email.com'};
+
+  NestedView.registerHandlebars(Handlebars);
+
+  templates['outer_view'] = Handlebars.compile(
+    '<h1>Hello, {{user.name}}.</h1>' +
+    '{{view "inner_view" arg1="string" arg2="true" context=user}}'
+  );
+
+  templates['inner_view'] = Handlebars.compile(
+   'Name: {{name}}<br>' +
+   'Email: {{email}}'
+  );
+
+  var BaseNestedView = NestedView.extend({
+    getTemplate: function(){
+      return templates[this.name];
+    }
+  });
+
+  NestedView.getView = function(name) {
+    return views[name];
+  };
+
+  views['outer_view'] = BaseNestedView.extend({
+    name: 'outer_view',
+    tagName: 'section',
+    className: 'outer'
+  });
+
+  views['inner_view'] = BaseNestedView.extend({
+    name: 'inner_view',
+    className: 'inner'
+  });
+
+  describe('NestedView', function(){
+
+    describe('viewHelper', function(){
+      it('should register the Handlebars helper', function(){
+        assert.equal(Handlebars.helpers.view, NestedView.viewHelper);
+      });
+    });
+
+    describe('getTemplateData', function(){
+      it('should return the options if no model or collection', function(){
+        var innerView = new views['inner_view'](userData);
+
+        assert.deepEqual(innerView.getTemplateData(), userData);
+      });
+
+      it('should return the model\'s toJSON() if a model is present', function(){
+        var data = {model: new Backbone.Model(userData)}
+          , innerView = new views['inner_view'](data);
+
+          assert.deepEqual(innerView.getTemplateData(), userData);
+      });
+
+      it('should return the collection\'s toJSON() if a collection is present', function(){
+        var data = {collection: new Backbone.Collection([userData, userData])}
+          , innerView = new views['inner_view'](data);
+
+          assert.deepEqual(innerView.getTemplateData(), {collection: [userData, userData]});
+      });
+    });
+
+    describe('getHtml()', function(){
+      it('should return the outerHtml of a single view', function(){
+        var innerView = new views['inner_view'](userData)
+          , html = innerView.getHtml();
+
+        assert.equal(html, '<div class="inner" data-view="inner_view">Name: Spike<br>Email: spike@email.com</div>');
+      });
+
+      it('should return the innerHtml of a single view', function(){
+        var innerView = new views['inner_view'](userData)
+          , html = innerView.getHtml({outerHtml: false});
+
+        assert.equal(html, 'Name: Spike<br>Email: spike@email.com');
+      });
+
+      it('should return the html of a nested view', function(){
+        var data = {user: userData}
+          , outerView = new views['outer_view'](data)
+          , html = outerView.getHtml();
+
+        assert.equal(html,
+          '<section class="outer" data-view="outer_view"><h1>Hello, Spike.</h1>' +
+          '<div class="inner" data-view="inner_view">Name: Spike<br>Email: spike@email.com</div></section>'
+        );
+      });
+    });
+
+    describe('childViews', function(){
+      it('should provide access to child view instances', function(){
+        var data = {user: userData}
+          , outerView = new views['outer_view'](data)
+          , innerView = outerView.childViews['inner_view'];
+
+        assert(innerView instanceof views['inner_view']);
+        assert.deepEqual(innerView.options, data.user);
+      });
+    });
+
+  });
+})();
